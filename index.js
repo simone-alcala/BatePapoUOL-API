8ef9774622ae77d8cc71b73de0783769aee1c077
simone-alcala <simone.alcala@gmail.com>
2022-04-29 14:19:40 -0300

    Fixed error messages

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+const schemaName    = Joi.object({ name: Joi.string().required().trim() });
+const schemaUser    = Joi.object({ user: Joi.string().required().trim() });
+const schemaMessage = Joi.object({
+  to:   Joi.string().required().trim(),
+  text: Joi.string().required().trim(),
+  type: Joi.string().required().valid('message','private_message')
+});
+
 const options = {
   abortEarly: false, // include all errors
   allowUnknown: true, // ignore unknown props
@@ -22,11 +30,17 @@ const options = {
 }
 
 function showErrorMessages(e,res){
-  if (e.name === 'TypeError') return res.sendStatus(500);
-    else if (e.name === 'ValidationError' || e === 'notInserted' || e === 'notUpdated') return res.sendStatus(422);
-    else if(e === 'alreadyRegistered') return res.sendStatus(409);
-    else if(e === 'notFound') return res.sendStatus(404);
-    else return res.status(500).send(e);
+
+  if (e.name === 'ValidationError'){
+    res.status(422).send(e.details); 
+  } else if (e.name === 'alreadyRegistered') {
+    res.status(409).send(e.details); 
+  } else if (e.name === 'notFound') {
+    res.status(404).send(e.details); 
+  } else {
+    res.status(500).send(e); 
+  }
+
 }
 
 function verifyActiveUsers (){
@@ -67,17 +81,24 @@ async function deleteInactiveUsers(){
 }
 
 app.post('/participants',async(req,res) => {
-  const schema = Joi.object({ name: Joi.string().required().trim() });
+
   try {    
-    const value = await schema.validateAsync(req.body,options);
+
+    const value = await schemaName.validateAsync(req.body,options);
+
     const participant = {
       name: value.name,
       lastStatus: Date.now()
     };
+
     await mongoClient.connect();
+
     const db = mongoClient.db(dbName);
+
     const alreadyRegistered = await db.collection('participants').findOne({name: participant.name});
-    if (alreadyRegistered) throw new Error().message = 'alreadyRegistered';
+    
+    if (alreadyRegistered) 
+      throw { name: 'alreadyRegistered' , details: `User ${participant.name} already registered`} ;
 
     const message = {
       from: participant.name,
@@ -87,45 +108,56 @@ app.post('/participants',async(req,res) => {
       time: dayjs().format('HH:mm:ss')
     };
 
-    const insertedParticipant = await db.collection('participants').insertOne(participant);
-    const insertedMessage = await db.collection('messages').insertOne(message);
+    const newParticipant = await db.collection('participants').insertOne(participant);
 
-    if  (!(insertedParticipant.acknowledged && insertedMessage.acknowledged) )
-      throw new Error().message = 'notInserted';
-    res.sendStatus(201);
-    mongoClient.close();
+    if (newParticipant.acknowledged){
+
+      const newMessage = await db.collection('messages').insertOne(message);
+
+      if (newMessage.acknowledged){
+        res.sendStatus(201);
+        mongoClient.close();
+      }
+    }
+
   } catch (e) {
-    mongoClient.close();
+
     console.log(e);   
+
     showErrorMessages(e,res);
+
+    mongoClient.close();
+   
   }  
 });
 
 app.get('/participants',async(req,res) => {
+  
   try {
+
     await mongoClient.connect();
     const db = mongoClient.db(dbName);
+
     const participants = await db.collection('participants').find({}).toArray();
+
     res.send(participants);
+
     mongoClient.close();
+
   } catch (e) {
-    console.error(e);
-    res.sendStatus(500);
+
+    console.log(e);
+    res.status(500).send(e);
     mongoClient.close();
-  }
+  } 
+  
 });
 
-app.post('/messages',async(req,res) => {
-  const schema = Joi.object({
-    to:   Joi.string().required().trim(),
-    text: Joi.string().required().trim(),
-    type: Joi.string().required().valid('message','private_message')
-  });
-  const schemaUser = Joi.object({
-    user: Joi.string().required().trim(),
-  });
+app.post('/messages',async(req,res) => { 
+  
   try {
-    const value = await schema.validateAsync(req.body,options);
+
+    const value = await schemaMessage.validateAsync(req.body,options);
     const valueUser = await schemaUser.validateAsync(req.headers,options);
 
     await mongoClient.connect();
@@ -133,8 +165,9 @@ app.post('/messages',async(req,res) => {
     
     const participant = await db.collection('participants').findOne({name: valueUser.user});
 
-    if (!participant) throw new Error().message = 'notFound'; //** VERIFICAR SE PŔECISA DESSA VALIDAÇÃO **/;
-    
+    if (!participant) 
+      throw { name: 'notFound' , details: `User ${valueUser.user} not found`}; //** VERIFICAR SE PŔECISA DESSA VALIDAÇÃO **/
+        
     const message = {
       to: value.to,
       from: valueUser.user,
@@ -143,30 +176,26 @@ app.post('/messages',async(req,res) => {
       time: dayjs().format('HH:mm:ss')
     }
 
-    const insertedMessage = await db.collection('messages').insertOne(message); 
-
-    if (!insertedMessage.acknowledged) throw new Error().message = 'notInserted';
+    await db.collection('messages').insertOne(message); 
 
     res.sendStatus(201);
+
     mongoClient.close();
 
   } catch (e) {
-
-    console.error(e);
-    mongoClient.close();
+    console.log(e);   
     showErrorMessages(e,res);
-
+    mongoClient.close();
   }
 });
 
 app.get('/messages',async(req,res) => {
-
-  const schema = Joi.object({ user: Joi.string().required().trim()  });
+ 
   const limit = req.query.limit;
 
   try {
 
-    const value = await schema.validateAsync(req.headers,options);
+    const value = await schemaUser.validateAsync(req.headers,options);
     
     await mongoClient.connect();
     const db = mongoClient.db(dbName);
@@ -180,12 +209,15 @@ app.get('/messages',async(req,res) => {
           {type: 'message'} ] }).toArray();
     
     const messagesToReturn = !limit ? messages.slice(parseInt(limit) * (-1) ) : messages.slice();
+       
     res.send(messagesToReturn);
+
     mongoClient.close();
+
   } catch (e) {
-    mongoClient.close();
     console.log(e);   
-    showErrorMessages(e,res);   
+    showErrorMessages(e,res);
+    mongoClient.close();
   }
 });
 
@@ -199,31 +231,29 @@ app.put('/messages/:id',(req,res) => {
 
 app.post('/status',async(req,res) => {
 
-  const schema = Joi.object({ user: Joi.string().required().trim() });
-
   try {
     
-    const value = await schema.validateAsync(req.headers,options);
+    const value = await schemaUser.validateAsync(req.headers,options);
 
     await mongoClient.connect();
     const db = mongoClient.db(dbName);
 
     const participant = await db.collection('participants').findOne({name: value.user});
 
-    if (!participant) throw new Error().message = 'notFound';
+    if (!participant) 
+      throw { name: 'notFound' , details: `User ${valueUser.user} not found`}; 
 
     const updated = await db.collection('participants').updateOne( 
       {_id: new ObjectId(participant._id)},  { $set: { lastStatus: Date.now() } } ); 
-
-    if (!updated.acknowledged) throw new Error().message = 'notUpdated';
-
+     
     res.sendStatus(200);
 
     mongoClient.close();
+
   } catch (e) {
-    mongoClient.close();
     console.log(e);   
-    showErrorMessages(e,res);  
+    showErrorMessages(e,res);
+    mongoClient.close();
   }
 
 });
